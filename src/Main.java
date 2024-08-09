d6eafdc1f88e2e2699c8de2d70dd5213c2d2bcd4
ahmad1542 <ahmad.husien4@gmail.com>
2024-08-09 22:56:40 +0300

    now to linked list

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Queue <Integer> qNumbers = new LinkedList<>();		
 		
+		qNumbers.add(10);
+		qNumbers.add(20);
+		qNumbers.add(30);
+		qNumbers.add(40);
+		
+		System.out.println("Count = " + qNumbers.size());
+
+		while (!qNumbers.isEmpty()) {
+			System.out.println(qNumbers.poll());
+		}
 	}
 	
 }
